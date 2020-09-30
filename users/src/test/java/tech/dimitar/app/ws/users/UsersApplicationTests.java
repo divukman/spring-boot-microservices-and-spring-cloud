fda56638b61dc442a0698a61723cf0d2882cbd85
Dimitar Vukman <divukman@gmail.com>
2020-09-30 15:07:14 +0100

    Users service

---
+++ users/src/test/java/tech/dimitar/app/ws/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimitar.app.ws.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
