bb3e6b6a52ccaf65b117bcc0225ce0ebc1f65dd5
Dimitar Vukman <divukman@gmail.com>
2020-10-02 11:56:49 +0100

    ZUUL Proxy - API Gateway

---
+++ apigateway/src/test/java/tech/dimitar/app/ws/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimitar.app.ws.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
